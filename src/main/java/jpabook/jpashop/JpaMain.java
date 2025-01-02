2335ebe6b3a5c2e35491717ab9d740611d3c8390
gyuhyuk <bruno3@naver.com>
2025-01-02 19:30:02 +0900

    section 8-1 : 상속관계 매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Movie;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -16,13 +17,18 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = new Order();
-            em.persist(order);
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
 
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
+            em.persist(movie);
 
-            em.persist(orderItem);
+            em.flush();
+            em.clear();
+
+            em.find(Movie.class, movie.getId());
 
             tx.commit();
         } catch (Exception e) {
---
+++ src/main/java/jpabook/jpashop/domain/Album.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("A")
+public class Album extends Item {
+    private String artist;
+}
